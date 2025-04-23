2ca94435445ffd2371369555f6bda5ae53e10138
Joaquín Marín <73167806+jmargar217@users.noreply.github.com>
2025-04-23 14:17:27 +0200

    WIP API Footbal Conference

---
+++ APIFootbalConference/src/test/java/com/football/conference/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
